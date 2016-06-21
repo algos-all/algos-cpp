3e4872e73b25abed3bec7b5be70e08569a8e67eb
Alexander Lisianoi <all3fox@gmail.com>
2016-06-21 16:18:35 +0200

    Binary search: use const auto instead of iterator

--- src/binsearch.hpp
+++ src/binsearch.hpp
@@ -1,13 +1,13 @@
 #include <iterator>
 
-template<typename Iterator, typename T>
-Iterator binsearch(Iterator begin, Iterator end, const T &v) {
+template<typename iterator, typename T>
+iterator binsearch(iterator begin, iterator end, const T &v) {
     if (begin == end) {return end;}
 
-    Iterator save = end;
+    const auto save = end;
 
     while (begin != end) {
-        Iterator mid = begin + std::distance(begin, end) / 2;
+        const auto mid = begin + std::distance(begin, end) / 2;
 
         if (*mid == v) {
             return mid;
