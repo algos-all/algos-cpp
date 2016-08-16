d19c96f8ad134d447268d12137de5acdde6fd044
Alexander Lisianoi <all3fox@gmail.com>
2016-08-16 21:32:21 +0300

    Mergesort: drop iterator constness

--- src/sort/mergesort.hpp
+++ src/sort/mergesort.hpp
@@ -8,11 +8,13 @@ template<
         typename std::iterator_traits<InputIt>::value_type
     >
 >
-void mergesort(const InputIt ix, const InputIt iy, Key key=Key())
-{
-    if (ix == iy || ix + 1 == iy) {return;}
+void mergesort(InputIt ix, InputIt iy, Key key=Key()) {
+    if (ix == iy || ix + 1 == iy) {
+        return;
+    }
 
     auto iz = std::next(ix, std::distance(ix, iy) / 2);
+
     mergesort(ix, iz, key);
     mergesort(iz, iy, key);
 
