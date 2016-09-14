4e03373722f1d06abc7154ca7b975cf1628620ac
Alexander Lisianoi <all3fox@gmail.com>
2016-09-15 00:54:02 +0200

    Sieve of Eratosthenes: minor style changes

--- src/esieve.hpp
+++ src/esieve.hpp
@@ -2,15 +2,15 @@
 
 template <typename T>
 std::vector<T> esieve(const T &n) {
-    auto primes = std::vector<T>(n, 0);
+    auto xs = std::vector<T>(n, 0);
 
     for (T i = 2; i < n; ++i) {
-        if (primes[i] != 0) {continue;}
+        if (xs[i] != 0) continue;
 
         for (T j = i; j < n; j += i) {
-            primes[j] = i;
+            xs[j] = i;
         }
     }
 
-    return primes;
+    return xs;
 }
