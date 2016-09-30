647b18520cd6292d38557dd8b3cfe38b75d52c94
Alexander Lisianoi <all3fox@gmail.com>
2016-09-30 12:28:23 +0200

    Mergesort: implement a version that uses O(N) extra space
    Adapt unit tests to use the "seamless scaling" introduced before

--- src/sort/mergesort.hpp
+++ src/sort/mergesort.hpp
@@ -2,16 +2,51 @@
 #include <algorithm>
 #include <functional>
 
-template<typename RandomIt, typename Key=std::less<>>
-void mergesort(RandomIt ix, RandomIt iy, Key key=Key{}) {
+
+namespace detail {
+template<class RandomIt, class Key=std::less<>>
+void mergesort0(RandomIt ix, RandomIt iy, Key key=Key{}) {
     if (ix == iy || std::next(ix) == iy) {
         return;
     }
 
-    auto it = std::next(ix, std::distance(ix, iy) / 2);
+    auto mid = std::next(ix, std::distance(ix, iy) / 2);
 
-    mergesort(ix, it, key);
-    mergesort(it, iy, key);
+    mergesort0(ix, mid, key);
+    mergesort0(mid, iy, key);
 
-    std::inplace_merge(ix, it, iy, key);
+    std::inplace_merge(ix, mid, iy, key);
+}
+
+template<class RandomIt, class Key=std::less<>>
+void mergesort1(RandomIt fst, RandomIt lst, Key key=Key{}) {
+    if (fst == lst || std::next(fst) == lst) return;
+
+    auto mid = std::next(fst, std::distance(fst, lst) / 2);
+
+    mergesort1(fst, mid, key);
+    mergesort1(mid, lst, key);
+
+    std::vector<
+        typename std::iterator_traits<RandomIt>::value_type
+    > tmp(fst, lst);
+
+    mid = std::next(tmp.begin(), std::distance(fst, lst) / 2);
+    std::merge(tmp.begin(), mid, mid, tmp.end(), fst, key);
+}
+} // namespace detail
+
+
+template<class RandomIt, class Key=std::less<>>
+void mergesort0(RandomIt begin, RandomIt end, Key key=Key{}) {
+    if (begin >= end) return;
+
+    detail::mergesort0(begin, end, key);
+}
+
+template<class RandomIt, class Key=std::less<>>
+void mergesort1(RandomIt begin, RandomIt end, Key key=Key{}) {
+    if (begin >= end) return;
+
+    detail::mergesort1(begin, end, key);
 }
