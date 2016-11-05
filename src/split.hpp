524798636523a39f22cdf88d07c6a2d604511ced
Alexander Lisianoi <all3fox@gmail.com>
2016-11-05 13:49:14 +0100

    Update split to use do .. while

--- src/split.hpp
+++ src/split.hpp
@@ -8,15 +8,13 @@ vector<string> split(const string& str, const string& sep=" ") {
     auto result = vector<string>();
     string::size_type lft = 0, rgt = 0;
 
-    while ((rgt = str.find(sep, lft)) != string::npos) {
+    do {
+        rgt = str.find(sep, lft);
+
         result.push_back(str.substr(lft, rgt - lft));
 
         lft = rgt + sep.size();
-    }
-
-    if (lft != rgt) {
-        result.push_back(str.substr(lft, rgt - lft));
-    }
+    } while (rgt != string::npos);
 
     return result;
 }
