030d54c8d5ac1ecaa023974ed7f3e5d539706ea2
Alexander Lisianoi <all3fox@gmail.com>
2016-11-05 20:15:55 +0100

    Make naming consistent in mergesort

--- src/sort/mergesort.hpp
+++ src/sort/mergesort.hpp
@@ -5,24 +5,24 @@
 
 namespace detail {
 template<class RandomIt, class Key=std::less<>>
-void mergesort0(RandomIt ix, RandomIt iy, Key key=Key{}) {
-    if (ix == iy || std::next(ix) == iy) {
-        return;
-    }
+void mergesort0(RandomIt fst, RandomIt lst, Key key=Key{}) {
+    if (fst == lst || std::next(fst) == lst) return;
 
-    auto mid = std::next(ix, std::distance(ix, iy) / 2);
+    auto mid = std::next(fst, std::distance(fst, lst) / 2);
 
-    mergesort0(ix, mid, key);
-    mergesort0(mid, iy, key);
+    mergesort0(fst, mid, key);
+    mergesort0(mid, lst, key);
 
-    std::inplace_merge(ix, mid, iy, key);
+    std::inplace_merge(fst, mid, lst, key);
 }
 
 template<class RandomIt, class Key=std::less<>>
 void mergesort1(RandomIt fst, RandomIt lst, Key key=Key{}) {
     if (fst == lst || std::next(fst) == lst) return;
 
-    auto mid = std::next(fst, std::distance(fst, lst) / 2);
+    const auto dst = std::distance(fst, lst);
+
+    auto mid = std::next(fst, dst / 2);
 
     mergesort1(fst, mid, key);
     mergesort1(mid, lst, key);
@@ -31,22 +31,22 @@ void mergesort1(RandomIt fst, RandomIt lst, Key key=Key{}) {
         typename std::iterator_traits<RandomIt>::value_type
     > tmp(fst, lst);
 
-    mid = std::next(tmp.begin(), std::distance(fst, lst) / 2);
+    mid = std::next(tmp.begin(), dst / 2);
     std::merge(tmp.begin(), mid, mid, tmp.end(), fst, key);
 }
 } // namespace detail
 
 
 template<class RandomIt, class Key=std::less<>>
-void mergesort0(RandomIt begin, RandomIt end, Key key=Key{}) {
-    if (begin >= end) return;
+void mergesort0(RandomIt fst, RandomIt lst, Key key=Key{}) {
+    if (fst == lst) return;
 
-    detail::mergesort0(begin, end, key);
+    detail::mergesort0(fst, lst, key);
 }
 
 template<class RandomIt, class Key=std::less<>>
-void mergesort1(RandomIt begin, RandomIt end, Key key=Key{}) {
-    if (begin >= end) return;
+void mergesort1(RandomIt fst, RandomIt lst, Key key=Key{}) {
+    if (fst == lst) return;
 
-    detail::mergesort1(begin, end, key);
+    detail::mergesort1(fst, lst, key);
 }
