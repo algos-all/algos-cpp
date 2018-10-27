50455b916c33fc1f93473daa2035c8672435c4a8
Aleksandr Lisianoi <alisianoi@protonmail.com>
2018-10-27 11:20:31 +0200

    Make src/dsu.hpp pass cpplint

--- src/dsu.hpp
+++ src/dsu.hpp
@@ -1,11 +1,16 @@
+#ifndef SRC_DSU_HPP_
+#define SRC_DSU_HPP_
+
 #include <initializer_list>
 #include <unordered_map>
 #include <iostream>
+#include <utility>
 
 template <class T>
 struct DisjointSetUnion {
     using S = std::size_t;
 
+    S count;
     std::unordered_map<T, T> xs;
     std::unordered_map<T, S> ws;
 
@@ -13,6 +18,8 @@ struct DisjointSetUnion {
 
     template <class RandomIt>
     DisjointSetUnion(RandomIt ix, RandomIt iy) {
+        count = std::distance(iy, ix);
+
         for (auto it = ix; it != iy; ++it) {
             xs[*it] = *it;
             ws[*it] = 1;
@@ -20,18 +27,22 @@ struct DisjointSetUnion {
     }
 
     DisjointSetUnion(std::initializer_list<T> keys) {
-        for (auto key: keys) {
+        count = keys.size();
+
+        for (auto key : keys) {
             xs[key] = key;
             ws[key] = 1;
         }
     }
 
     void push_back(const T& key) {
+        ++count;
         xs[key] = key;
         ws[key] = 1;
     }
 
-    T find(const T& key, const S w=0) {
+
+    T find(const T& key, const S w = 0) {
         if (xs[key] == key) {
             ws[key] += w;
 
@@ -48,8 +59,11 @@ struct DisjointSetUnion {
 
         if (val1 == val2) return;
 
-        if (ws[val1] < ws[val2])
+        --count;
+
+        if (ws[val1] < ws[val2]) {
             std::swap(val1, val2);
+        }
 
         xs[val2] = val1;
         ws[val2] += ws[val1];
@@ -58,11 +72,11 @@ struct DisjointSetUnion {
 
 template<class T>
 std::ostream& operator<<(std::ostream& os, const DisjointSetUnion<T>& dsu) {
-    for (auto x: dsu.xs) {
+    for (auto x : dsu.xs) {
         os << "(" << x.first << ", " << x.second << ") ";
     } os << std::endl;
 
-    for (auto w: dsu.ws) {
+    for (auto w : dsu.ws) {
         os << "(" << w.first << ", " << w.second << ") ";
     } os << std::endl;
 
@@ -70,3 +84,5 @@ std::ostream& operator<<(std::ostream& os, const DisjointSetUnion<T>& dsu) {
 
     return os;
 }
+
+#endif  // SRC_DSU_HPP_
